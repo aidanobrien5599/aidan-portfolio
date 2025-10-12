5b6bb13fbda9be54eaf56c37df60795959a640c0
Aidan O'Brien <aob55992@gmail.com>
2025-10-12 16:26:56 -0500

    done here

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -16,13 +16,13 @@ export const Badge = ({
     <a
       href={download}
       download
-      className="bg-slate-900 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block"
+      className="bg-green-700 hover:bg-green-800 no-underline group cursor-pointer relative shadow-lg shadow-green-700/20 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block transition-all duration-300 hover:shadow-xl hover:shadow-green-700/30"
       {...props}
     >
       <span className="absolute inset-0 overflow-hidden rounded-full ">
-        <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+        <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(21,128,61,0.4)_0%,rgba(21,128,61,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
       </span>
-      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-transparent py-2 px-4 ring-1 ring-white/10 ">
+      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-transparent py-2 px-4 ring-1 ring-green-600/15 ">
         <span>{text}</span>
         {icon ?? (
           <svg
@@ -45,7 +45,7 @@ export const Badge = ({
           </svg>
         )}
       </div>
-      <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+      <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-green-600/0 via-green-600/70 to-green-600/0 transition-opacity duration-500 group-hover:opacity-50"></span>
     </a>
   );
 };
